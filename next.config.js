61455ca9260466345e9cb817e555d62e5ad88828
Malachi <malachicadenpadilla@gmail.com>
2023-03-11 21:11:13 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
 	env: {
 		BASE_URL: process.env.BASE_URL,
 	},
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
 };
 module.exports = {
 	webpack(config) {
